14e5ff2ce2d2609f524cc5999fb993ec6f5d8f3d
Michelle Tumur <milk@unc.edu>
2023-01-22 20:31:37 -0500

    Progress on Exercise 1

--- exercises/ex00_hello_world.py
+++ exercises/ex00_hello_world.py
@@ -1,3 +1,3 @@
-"""My first program for COMP110"""
+"""My first program for COMP110."""
 __author__ = "730525708"
 print("Hello, world.")
---
+++ exercises/ex01_chardle.py
@@ -0,0 +1,41 @@
+"""EX01 - Chardle - A cute step toward Wordle."""
+
+__author__ = "730525708"
+    
+solution = input("Enter a 5-character word: ")
+if len(solution) != 5:
+    print("Error: Word must contain 5 characters")
+    exit()
+
+letter = input("Enter a single chracter: ")
+if len(letter) != 1:
+    print("Error: Character must be a single character")
+    exit()
+    
+print("searching for " + letter + " in " + solution)
+instance = 0
+
+if letter == solution[0]:
+    print(letter + " found at index 0")
+    instance += 1
+if letter == solution[1]:
+    print(letter + " found at index 1")
+    instance += 1
+if letter == solution[2]:
+    print (letter + " found at index 2")
+if letter == solution[3]:
+    print(letter + "found at index 3")
+if letter == solution[4]:
+    print(letter + "found at index 4")
+if instance == 1:
+    print(str(instance) + " instances of " + letter + " found in " + solution)
+if instance > 1:
+    print(str(instance) + " instances of " + letter + " found in " + solution)
+if instance == 0:
+    print("No instances of " + letter + " found in " + solution)
+
+
+
+
+
+
